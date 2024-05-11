18e40c3de201674323750d38e161db38a1e273f3
HendAdel <hend.adel.m@gmail.com>
2024-05-11 22:18:32 +0300

    Add images.

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,46 @@ void main() {
         body: Center(
           child: Column(
             children: [
-              Container(
+              
+              Container(                
+                padding: EdgeInsets.all(30),
                 
+                height: 100,
+                width: 500,
+                decoration:BoxDecoration(color: Colors.orange, borderRadius: BorderRadius.circular(15)),
+                child: Row(
+                  children: [  
+                    Image.asset('assets/vP.jpg', height: 80, width: 80,),
+                  SizedBox(width: 15,),
+                  Text('Vegtables pizze', style: TextStyle(color: Colors.black, fontSize: 25),)                    
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15,),
+              Container(                
                 padding: EdgeInsets.all(30),
                 height: 100,
                 width: 500,
                 decoration:BoxDecoration(color: Colors.orange, borderRadius: BorderRadius.circular(15)),
                 child: Row(
-                  children: [
-                    Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), 
-                    ),
-                    child: Text('Vegtables pizze', style: TextStyle(color: Colors.black, fontSize: 25),),
-                    child: Image.asset('assets/vP.jpg')),
+                  children: [  
+                    Image.asset('assets/images.jpg', height: 80, width: 80,),
+                  SizedBox(width: 15,),
+                  Text('cheese pizze', style: TextStyle(color: Colors.black, fontSize: 25),)                    
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15,),
+              Container(                
+                padding: EdgeInsets.all(30),
+                height: 100,
+                width: 500,
+                decoration:BoxDecoration(color: Colors.orange, borderRadius: BorderRadius.circular(15)),
+                child: Row(
+                  children: [  
+                    Image.asset('assets/fp.jpg', height: 80, width: 80,),
+                  SizedBox(width: 15,),
+                  Text('Fried potato', style: TextStyle(color: Colors.black, fontSize: 25),)                    
                   ],
                 ),
               ),
